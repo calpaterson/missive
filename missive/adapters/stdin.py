441bf5401d7472e10453fbb91c43d55b7e60c165
Cal Paterson <cal@calpaterson.com>
2020-03-03 14:03:49 +0000

    Improve the stdin adapter a bit

--- missive/adapters/stdin.py
+++ missive/adapters/stdin.py
@@ -1,13 +1,32 @@
+import os
+from select import select
 import sys
-from typing import Type
+from typing import Type, BinaryIO, Optional
+from logging import getLogger
+from io import BytesIO
 
-from ..missive import Adapter, Processor, M
+from missive import Adapter, Processor, M
+from missive.shutdown_handler import ShutdownHandler
+
+logger = getLogger(__name__)
+
+_CHUNK_SIZE = 4096
 
 
 class StdinAdapter(Adapter[M]):
-    def __init__(self, message_cls: Type[M], processor: Processor[M]) -> None:
+    def __init__(
+        self,
+        message_cls: Type[M],
+        processor: Processor[M],
+        filelike: Optional[BinaryIO] = None,
+    ) -> None:
         self.processor = processor
         self.message_cls = message_cls
+        if filelike is None:
+            self.filelike = sys.stdin.buffer
+        else:
+            self.filelike = filelike
+        self.shutdown_handler = ShutdownHandler()
 
     def ack(self, message: M) -> None:
         pass
@@ -16,7 +35,14 @@ class StdinAdapter(Adapter[M]):
         raise NotImplementedError("No nack for stdin")
 
     def run(self) -> None:
+        logger.info("started")
         with self.processor.handling_context(self.message_cls, self) as ctx:
-            for line in sys.stdin:
-                data = line.rstrip().encode("utf-8")
-                ctx.handle(self.message_cls(raw_data=data))
+            while not self.shutdown_handler.should_exit():
+                # FIXME: rewrite this to use selectors instead of low level select
+                select([self.filelike], [], [])
+                content: bytes = os.read(self.filelike.fileno(), _CHUNK_SIZE)
+                for line in content.splitlines():
+                    logger.info("got line")
+                    logger.info(line)
+                    data = line.rstrip()
+                    ctx.handle(self.message_cls(raw_data=data))
--- tests/adapters/test_binaryio.py
+++ tests/adapters/test_binaryio.py
@@ -1,13 +1,37 @@
+import json
+import threading
+import time
+import os
+
+import missive
 from missive.adapters.stdin import StdinAdapter
 
-import pytest
 
+def test_from_file():
+    r_pipe, w_pipe = os.pipe()
+    r_fl, w_fl = os.fdopen(r_pipe, mode="rb"), os.fdopen(w_pipe, mode="wb")
 
-@pytest.mark.xfail(reason="not implemented")
-def test_acking():
-    assert False
+    flag = False
 
+    processor = missive.Processor()
 
-@pytest.mark.xfail(reason="not implemented")
-def test_nacking():
-    assert False
+    @processor.handle_for([])
+    def catch_all(message, ctx):
+        nonlocal flag
+        flag = message.get_json()
+        ctx.ack(message)
+        adapted.shutdown_handler.set_flag()
+
+    adapted = StdinAdapter(missive.JSONMessage, processor, r_fl)
+    thread = threading.Thread(target=adapted.run)
+    thread.start()
+
+    test_event = {"test-event": True}
+
+    w_fl.write(json.dumps({"test-event": True}).encode("utf-8"))
+    w_fl.write(b"\n")
+    w_fl.flush()
+
+    thread.join()
+
+    assert flag == test_event
